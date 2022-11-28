d6fdc4be81b3038c1eaec02abf9dd892695ecd37
MCKishMish <pashabest032@gmail.com>
2022-11-28 21:18:40 +0300

    2 numbers math added

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите 1 число:");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите 2 число:");
+        int secondNumber = new Scanner(System.in).nextInt();
+        int sum = firstNumber+secondNumber;
+        int dif = firstNumber-secondNumber;
+        int product = firstNumber*secondNumber;
+        double quotient = (double)firstNumber/secondNumber;
+        System.out.println("Сумма чисел = " + sum);
+        System.out.println("Разность чисел = " + dif);
+        System.out.println("Произведение чисел = " + product);
+        System.out.println("Частное чисел = " + quotient);
     }
 }
